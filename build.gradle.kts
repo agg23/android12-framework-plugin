0d8df9c60e75df3153aa1b6ccb08904a1884a6e2
Adam Gastineau <adam@agg.im>
2025-06-25 19:41:57 -0700

    Copy android.jar into temp dir

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 group = "com.github.agg23"
-version = "1.0.1"
+version = "1.0.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
--- src/main/kotlin/SystemJarsPlugin.kt
+++ src/main/kotlin/SystemJarsPlugin.kt
@@ -1,21 +1,30 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import java.net.URL
+import org.gradle.api.plugins.JavaPlugin
+import java.io.File
+import java.io.InputStream
 
 class SystemJarsPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.logger.info("Applying SystemJarsPlugin")
 
-        // Get android.jar resource URL from plugin classloader
-        val androidJarUrl: URL? = this::class.java.classLoader.getResource("android.jar")
-        
-        if (androidJarUrl != null) {
-            // Create file reference directly from the resource URL
-            val androidJarRef = project.files(androidJarUrl.toURI())
+        // This isn't as elegant as it could be, but we want an easy to deploy option
+        // Get android.jar resource stream
+        val inputStream: InputStream? = this::class.java.classLoader.getResourceAsStream("android.jar")
+        if (inputStream != null) {
+            // Create temporary directory
+            val tempDir = File(project.buildDir, "system-jars")
+            tempDir.mkdirs()
+            
+            // Create temporary file
+            val tempFile = File(tempDir, "android.jar")
+            tempFile.outputStream().use { out ->
+                inputStream.copyTo(out)
+            }
             
             // Add to compileOnly configuration
-            project.dependencies.add("compileOnly", androidJarRef)
-            project.logger.info("Added android.jar to compileOnly configuration from plugin resources")
+            project.dependencies.add("compileOnly", project.files(tempFile))
+            project.logger.info("Added android.jar to compileOnly configuration from temporary file")
         } else {
             project.logger.error("android.jar not found in plugin resources. Plugin will not be applied.")
         }
